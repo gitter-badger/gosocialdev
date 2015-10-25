cfc4187f40e047b56c194929c9c6a1caeddea5e9
Rares Golea <rgolea@gmail.com>
2015-10-25 19:59:10 +0100

    Installed dependencies

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular.module('app', ['ngResource', 'ngAnimate', 'ngAria', 'ngMaterial'])
+    .controller('AppController', AppController);
+
+  function AppController() {
+    var ctrl = this;
+  }
+
+})();
